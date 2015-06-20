540ddda8d8bdb29aac58982c25fe8ba0f02bf857
Jeremy Stover <jeremy.ryan.stover@gmail.com>
2015-06-20 00:10:55 -0700

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weatherAppApp
+ * @description
+ * # weatherAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weatherAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/appConfig_example.js
@@ -0,0 +1,6 @@
+'use strict';
+var config = (function(){
+	return {
+		weatherApi: 'keyHere'
+	};
+}());
